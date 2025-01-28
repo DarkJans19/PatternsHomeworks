1f9eff7d4bfa169bee5e24cc1b3de128bde05940
DarkJans19 <jansito4221@gmail.com>
2025-01-28 17:44:48 -0500

    Added all the logic of the program

---
+++ EstructuralPatterns/flyweight/src/main/java/com/example/TreeFactory.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TreeFactory {
+    private static Map<String, TreeType> treeTypes = new HashMap<>();
+
+    public static TreeType getTreeType(String name, String color, String texture){
+        TreeType type = treeTypes.get(name);
+        if(type == null){
+            type = new ConcreteTreeFile(name, color, texture);
+            treeTypes.put(texture, type);
+        }
+        return type;
+    }
+}
